8e3d375973b1a0bbabffeffe3c8790664db1ce7a
Daniel Guerrero <danielguerrero@dguerrm3-mbp16.local>
2024-05-11 17:19:57 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'time-accounting-test',
+  webDir: 'www'
+};
+
+export default config;
